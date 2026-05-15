f470fa1c29011020cf1520a5b908a4d69d61b0e3
Daniela <daniela_fragoso@hotmail.com>
2026-05-15 17:10:54 +0100

    JavaScript added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("Hello World");
+function searchCity(event) {
+  event.preventDefault();
+  let formInput = document.querySelector("#form-input");
+  console.log(document.querySelector("#form-input"));
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
