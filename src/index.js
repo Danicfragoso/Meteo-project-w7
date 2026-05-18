7c0985a31d64a396b408927f3ca9183b1ba7ad0a
Daniela <daniela_fragoso@hotmail.com>
2026-05-18 12:01:18 +0100

    small changes on javascript and small bugs corrections

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-function searchCity(event) {
+function updateData(response) {
+
+  let temperatureValue = document.querySelector("#weather-value");
+   temperatureValue.innerHTML = Math.round(response.data.temperature.current);
+     console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "ae33a3fba9364t0co66343008146ece1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateData);
+}
+
+function changeCity(event) {
   event.preventDefault();
   let formInput = document.querySelector("#form-input");
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = formInput.value;
-
- 
+  searchCity(formInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", changeCity);
+
