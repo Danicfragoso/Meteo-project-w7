926173c2374e53d3df3a7244f884cc6e24bf6573
Daniela <daniela_fragoso@hotmail.com>
2026-05-18 17:16:17 +0100

    Added Weather data on JavaScript

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ function updateData(response) {
    temperatureValue.innerHTML = Math.round(response.data.temperature.current);
    let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let timeElement =document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+
+}
+function formatDate (date) {
+  let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+ if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+  return `${day}, ${hours}:${minutes}`;
+
 }
 
 function searchCity(city) {
@@ -15,11 +36,9 @@ function searchCity(city) {
 function changeCity(event) {
   event.preventDefault();
   let formInput = document.querySelector("#form-input");
- 
   searchCity(formInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", changeCity);
-
 searchCity ("Lisbon");
